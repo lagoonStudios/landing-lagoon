f4be49c869b63a6a32723719e7499ee8900e6153
Leonardo Contreras <joseleoc123@gmail.com>
2024-04-16 17:03:36 -0400

    fix eslint build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
     extends: [
       'eslint:recommended',
       'eslint-config-standard',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
       'plugin:astro/recommended',
       'plugin:prettier/recommended',
     ],
@@ -30,6 +32,19 @@ module.exports = {
       ],
     },
     overrides: [
+      {
+        files: ['*.astro'],
+        parser: 'astro-eslint-parser',
+        parserOptions: {
+          parser: '@typescript-eslint/parser',
+          extraFileExtensions: ['.astro'],
+        },
+      },
+      {
+        files: ['*.ts', '*.tsx'],
+        parser: '@typescript-eslint/parser',
+        extends: ['plugin:@typescript-eslint/recommended'],
+      },
       {
         // Define the configuration for `<script>` tag.
         // Script in `<script>` is assigned a virtual file name with the `.js` extension.

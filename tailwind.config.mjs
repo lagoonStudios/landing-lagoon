d5b57f99a0b5c3e182eaa63ba360d1fb6f37b730
Leonardo Contreras <joseleoc123@gmail.com>
2024-04-16 14:06:03 -0400

    hero page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				purple: '#A408FF',
-				blue: '#1760EC',
-				green:'#2FF5A3'
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#A408FF',
+        blue: '#1760EC',
+        green: '#2FF5A3',
+        'almost-white': '#EBF0F8',
+      },
+    },
+  },
+  plugins: [],
 }
